667edd06557e2860ef088460c265e92fe914065a
Justice Perez White <justicepwhite@gmail.com>
2018-05-10 13:04:04 -0500

    process of configuring webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,19 @@ const config = {
       path.resolve('./node_modules'),
     ]
   },
-  entry: ['babel-polyfill','./lib/renderers/dom.js'],
+  // entry: ['babel-polyfill','./lib/renderers/dom.js'],
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby'
+    ],
+    app: ['./lib/renderers/dom.js'],
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
